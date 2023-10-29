2bc6fea987a3c4aa92485603afccc4e8cf154e66
benwin22 <145385526+benwin22@users.noreply.github.com>
2023-10-29 12:18:19 -0500

    commit again

---
+++ calc1.py
@@ -0,0 +1,14 @@
+ #1) Create a Module in VS Code and Import It into jupyter notebook
+# Module should have the following capabilities:
+
+# 1a) Has a function to calculate the square footage of a house
+# Reminder of Formula: Length X Width == Area
+# 1b) Has a function to calculate the circumference of a circle 2 Pi r
+
+# Program in Jupyter Notebook should take in user input and use imported functions to calculate a circle's circumference or a houses square footage
+
+
+def area(x,y):
+    return (x*y)
+def circ(r):
+    return(r * 2 * 3.14)
